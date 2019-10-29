9eb00686856b29fdf7afc108648c657c23890e4a
agrawalram020 <agrawalram020@gmail.com>
2019-10-29 12:40:47 +0530

    2nd PR

--- tests/test_probability.py
+++ tests/test_probability.py
@@ -7,3 +7,5 @@ def test_trials():
     assert trials([0.6]) == [0.4, 0.6]
     assert trials([0.6,0.5]) == [0.2, 0.5, 0.3]
     assert trials([0.8,0.6,0.25]) == pytest.approx([0.06, 0.35, 0.47, 0.12])
+
+# my comment is
